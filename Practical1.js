dedd6cbbd274891f6179fc9fec0cb7af0ae7c7a5
Pavani Rathwaththe <pavanipabasara2118@gmail.com>
2025-03-12 15:39:54 +0530

    arithmetic operation

---
+++ Practical1.js
@@ -0,0 +1,16 @@
+console.log("Hello") //Prints Hello
+
+//arithmetic operations
+
+let num1 = 56
+let num2 = 65
+let sum = num1 + num2
+let sub = num2 - num1
+let mul = num1 * num2
+let div = num2 / num1			
+
+console.log(sum)	
+console.log(sub)
+console.log(mul)
+console.log(div)
+console.log()
